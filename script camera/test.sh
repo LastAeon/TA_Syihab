dc460c630cc014df624825b3875b379c8914ba17
Syihabuddin Yahya Muhammad <syihabuddiny@gmail.com>
2023-03-17 15:30:42 +0700

    tambah script camera

---
+++ script camera/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+host_moses="192.168.137.30"
+host_jose="192.168.137.156"
+host_wisnu="192.168.137.78"
+host_syihab="10.78.162.201"
+
+# start 
+echo "Vtes1" &
+sleep 1 &
+echo "Vtes2"&
+sleep 1 &
+echo "Vtes3"&
+sleep 1 &
+echo "Vtes4"&
+sleep 1 &
+echo "Vtes1" ; echo "Vtes2" ; echo "Vtes3" ; echo "Vtes4"
+
+# time for recording
+# sleep 2
+
+
+# stop 
