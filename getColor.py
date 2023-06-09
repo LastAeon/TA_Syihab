cc84d3746cd2fed42c82c64cf046aa325998cda5
Syihabuddin Yahya Muhammad <syihabuddiny@gmail.com>
2023-06-09 11:18:37 +0700

    update color for take 2

--- getColor.py
+++ getColor.py
@@ -4,11 +4,9 @@ import cv2
 import numpy as np
 
 
-myColorFinder = ColorFinder(1)
-hsvVals = {'hmin': 98, 'smin': 46, 'vmin': 90, 'hmax': 133, 'smax': 137, 'vmax': 255}
-# hsvVals = {'hmin': 45, 'smin': 80, 'vmin': 98, 'hmax': 56, 'smax': 140, 'vmax': 255}
-# {'hmin': 33, 'smin': 72, 'vmin': 126, 'hmax': 58, 'smax': 255, 'vmax': 255} # hijau
-# hsvVals = {'hmin': 6, 'smin': 42, 'vmin': 53, 'hmax': 32, 'smax': 206, 'vmax': 156} # kulit
+hsvVals = {'hmin': 140, 'smin': 41, 'vmin': 107, 'hmax': 162, 'smax': 148, 'vmax': 255}
+
+myColorFinder = ColorFinder(1, hsvVals)
 
 #define kernel size  
 kernel = np.ones((7,7),np.uint8)
@@ -16,7 +14,7 @@ if(1):
     while True:
         #This is to check whether to break the first loop
         isclosed=0
-        cap = cv2.VideoCapture('video/take2/kiri/VID_gerakan3.mp4')
+        cap = cv2.VideoCapture('video/take2/tengah/VID_gerakan3.mp4')
         # cap = cv2.VideoCapture(0)
         cap.set(3, 1280)
         cap.set(4, 720)
@@ -26,6 +24,7 @@ if(1):
             # h, w, _ = img.shape
             if np.shape(img) == ():
                  break
+            # img = cv2.resize(img, (5120, 2880))
             img = cv2.resize(img, (2560, 1440))
             # img = cv2.resize(img, (1280, 720))
             # img = cv2.resize(img, (640, 360))
