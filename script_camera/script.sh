f23ead8778e259d452194155d038ac323729345e
Syihabuddin Yahya Muhammad <syihabuddiny@gmail.com>
2023-06-17 09:55:29 +0700

    update script

--- script_camera/script.sh
+++ script_camera/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-host_moses="192.168.189.30"
-host_jose="192.168.189.156"
-host_wisnu="192.168.189.78"
+host_moses="192.168.115.30"
+host_jose="192.168.115.156"
+host_wisnu="192.168.115.78"
 host_syihab="10.178.253.93"
-
+vid_name="Sangle35rc"
 # start recording
-echo "Vgerakan5" | timeout 0.1 nc -u -b $host_moses 8000 &
-echo "Vgerakan5" | timeout 0.1 nc -u -b $host_jose 8000 
-# echo "Vgerakan5" | timeout 0.1 nc -u -b $host_wisnu 8000
+#echo $vid_name | timeout 0.1 nc -u -b $host_moses 8000 
+echo $vid_name | timeout 0.1 nc -u -b $host_jose 8000 &
+echo $vid_name | timeout 0.1 nc -u -b $host_wisnu 8000
 # echo "Vtes4" | timeout 0.1 nc -u -b $host_syihab 8000
 
 # # time for recording
